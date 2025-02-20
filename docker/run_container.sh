117a2149adb258a828d1bb8b4afc6453cb213a5e
Luc <luc001@e.ntu.edu.sg>
2025-02-20 15:42:47 +0100

    Added extensions from host

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,9 +1,12 @@
-docker run --rm -it \
+docker run -it \
     --network=host \
     --ipc=host \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $HOME/.vscode-server:/root/.vscode-server \
+    -v $HOME/.vscode-server/extensions:/root/.vscode-server/extensions \
+    -v $HOME/.vscode-server-insiders/extensions:/root/.vscode-server-insiders/extensions \
     -v $PWD:/home/smarc2user/colcon_ws \
     -v $PWD/src/smarc2/docker/vscode:/home/smarc2user/colcon_ws/.vscode \
     smarc2/base
