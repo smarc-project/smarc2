f741e816e587d2d114a86c2f26ee1b4c7f6154ae
ignaciotb <ignaciotorroba@hotmail.com>
2025-03-25 14:36:17 +0100

    bringup with use_sim_time exposed

--- scripts/smarc_bringups/scripts/sam_bringup.sh
+++ scripts/smarc_bringups/scripts/sam_bringup.sh
@@ -1,61 +1,73 @@
 #! /bin/bash
-ROBOT_NAME=sam_auv_v1
+#ROBOT_NAME=sam_auv_v1
+ROBOT_NAME=sam
+USE_SIM_TIME=false
+
+SIM=${USE_SIM_TIME}
 SESSION=${ROBOT_NAME}_bringup
 
 # create a tmux session with a name
 tmux -2 new-session -d -s $SESSION
-
 
 # create a bunch of windows. These are the "tabs" you'll
 # see at the bottom green line.
 # C-b <NUM> will change to the tab.
 # default window is 0
 
+# Conditional launches, for sim-only or real-only things
+# USE_SIM_TIME will be true for anything running in the sim, false for the real vehicles
+if [ $SIM = "true" ]
+then
+
+    # utility stuff like dubins planning and lat/lon conversions that other stuff rely on
+    tmux new-window -t $SESSION:5 -n 'utils'
+    tmux select-window -t $SESSION:5
+    tmux send-keys "ros2 launch smarc_bringups utilities.launch robot_name:=$ROBOT_NAME" C-m
+
+    # Mostly static stuff that wont be giving much feedback
+    # for robot description launch. so we get base_link -> everything else
+    tmux new-window -t $SESSION:8 -n 'description'
+    tmux select-window -t $SESSION:8
+    tmux send-keys "ros2 launch sam_description sam_description.launch robot_name:=$ROBOT_NAME" C-m
+    
+    # dummy stuff to temporarily let other stuff work
+    # tmux new-window -t $SESSION:9 -n 'dummies'
+    # tmux select-window -t $SESSION:9
+    # tmux send-keys "ros2 launch smarc_bringups dummies.launch robot_name:=$ROBOT_NAME" C-m
+
+else
+
+    # SAM's drivers and internal comms
+    tmux new-window -t $SESSION:0 -n 'core'
+    tmux select-window -t $SESSION:0
+    tmux send-keys "ros2 launch sam_drivers sam_core.launch robot_name:=$ROBOT_NAME" C-m
+
+fi    
+
 # state estimation stuff like pressure->depth, imu->tf etc
-tmux new-window -t $SESSION:0 -n 'dr'
-tmux rename-window "dr"
-# BT, action servers etc.
-tmux new-window -t $SESSION:1 -n 'bt'
-# controllers that are "constantly running"
-tmux new-window -t $SESSION:2 -n 'control'
-# connection to different GUIs
-tmux new-window -t $SESSION:3 -n 'gui'
-# utility stuff like dubins planning and lat/lon conversions that other stuff rely on
-tmux new-window -t $SESSION:4 -n 'utils'
-
-# for robot description launch. so we get base_link -> everything else
-tmux new-window -t $SESSION:8 -n 'description'
-# dummy stuff to temporarily let other stuff work
-tmux new-window -t $SESSION:9 -n 'dummies'
-
-
-# Now we launch things in each window.
-tmux select-window -t $SESSION:0
-#tmux send-keys "ros2 launch sam_dead_reckoning sam_dr_launch.launch robot_name:=$ROBOT_NAME" C-m
-tmux send-keys "echo 'Not launching sam_dead_reckoning sam_dr_launch.launch until someone fixes it!'" C-m
-
+tmux new-window -t $SESSION:1 -n 'dr'
 tmux select-window -t $SESSION:1
-tmux send-keys "ros2 launch smarc_bt smarc_bt.launch robot_name:=$ROBOT_NAME link_suffix:=_gt" C-m
+tmux send-keys "ros2 launch sam_dead_reckoning sam_dr_launch.launch robot_name:=$ROBOT_NAME use_sim_time:=$USE_SIM_TIME" C-m
+#tmux send-keys "echo 'Not launching sam_dead_reckoning sam_dr_launch.launch until someone fixes it!'" C-m
 
+# BT, action servers etc.
+tmux new-window -t $SESSION:2 -n 'bt'
 tmux select-window -t $SESSION:2
-tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME" C-m
+#tmux send-keys "ros2 launch smarc_bt smarc_bt.launch robot_name:=$ROBOT_NAME link_suffix:=_gt" C-m
+tmux send-keys "ros2 launch smarc_bt smarc_bt.launch robot_name:=$ROBOT_NAME use_sim_time:=$USE_SIM_TIME" C-m
 
+# controllers that are "constantly running"
+tmux new-window -t $SESSION:3 -n 'control'
 tmux select-window -t $SESSION:3
+#tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME tf_suffix:=_gt" C-m
+tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME use_sim_time:=$USE_SIM_TIME" C-m
+
+# connection to different GUIs
+tmux new-window -t $SESSION:4 -n 'gui'
+tmux select-window -t $SESSION:4
 tmux send-keys "ros2 launch smarc_nodered smarc_nodered.launch robot_name:=$ROBOT_NAME" C-m
 
-tmux select-window -t $SESSION:4
-tmux send-keys "ros2 launch smarc_bringups utilities.launch robot_name:=$ROBOT_NAME" C-m
 
-
-# Mostly static stuff that wont be giving much feedback
-tmux select-window -t $SESSION:8
-tmux send-keys "ros2 launch sam_description sam_description.launch robot_name:=$ROBOT_NAME" C-m
-
-tmux select-window -t $SESSION:9
-tmux send-keys "ros2 launch smarc_bringups dummies.launch robot_name:=$ROBOT_NAME" C-m
-
-
-# Conditional launches, for sim-only or real-only things
 # the real sam's username is "sam" and lolo's "lolo".
 # So we can switch on that.
 
@@ -68,6 +80,6 @@ then
 fi
 
 # Set default window
-tmux select-window -t $SESSION:1
+tmux select-window -t $SESSION:2
 # attach to the new session
 tmux -2 attach-session -t $SESSION
