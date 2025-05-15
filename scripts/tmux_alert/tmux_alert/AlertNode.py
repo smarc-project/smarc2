4f4a59ae507b507eb47ad5c604aab2edfe5d03ee
David <ddorner@kth.se>
2025-05-15 12:37:51 +0200

    Added launch file to the alert node

--- scripts/tmux_alert/tmux_alert/AlertNode.py
+++ scripts/tmux_alert/tmux_alert/AlertNode.py
@@ -1,13 +1,26 @@
+import sys
 import rclpy
 from rclpy.node import Node
+
 import subprocess
 import time
 import atexit
 import signal
 
-class TmuxAlertNode(Node):
-    def __init__(self):
-        super().__init__('tmux_alert_node')
+from smarc_msgs.msg import Topics as SMaRCTopics
+from sensor_msgs.msg import BatteryState
+
+class TmuxAlertNode():
+    def __init__(self, node):
+        #super().__init__('tmux_alert_node')
+
+        self._node = node
+
+        self._battery_percentage = None
+
+        self.state_sub = node.create_subscription(msg_type=BatteryState, topic=SMaRCTopics.BATTERY_TOPIC, callback=self._battery_cb, qos_profile=10)
+
+
 
         # Register shutdown cleanup
         atexit.register(self.reset_all_tmux_panes)
@@ -17,10 +30,14 @@ class TmuxAlertNode(Node):
         self.original_window_names = {}  # Store window_id -> name
         self.save_window_names()
 
-        self.timer = self.create_timer(1.0, self.check_condition)
+        self.timer = self._node.create_timer(1.0, self.check_condition)
+
+    def _battery_cb(self, msg):
+        self._battery_percentage = msg.percentage
+
 
     def handle_signal(self, signum, frame):
-        self.get_logger().info(f"Received signal {signum}, cleaning up...")
+        self._node.get_logger().info(f"Received signal {signum}, cleaning up...")
         self.reset_all_tmux_panes()
         rclpy.shutdown()
 
@@ -36,14 +53,14 @@ class TmuxAlertNode(Node):
             pane_ids = result.stdout.strip().split("\n")
             for pane_id in pane_ids:
                 subprocess.call(["tmux", "select-pane", "-t", pane_id, "-P", "default"])
-            self.get_logger().info("Reset all tmux pane colors to default.")
+            self._node.get_logger().info("Reset all tmux pane colors to default.")
         except subprocess.CalledProcessError as e:
-            self.get_logger().error(f"Failed to reset tmux panes: {e.stderr}")
+            self._node.get_logger().error(f"Failed to reset tmux panes: {e.stderr}")
 
         # Reset window names
         for win_id, original_name in self.original_window_names.items():
             subprocess.call(["tmux", "rename-window", "-t", win_id, original_name])
-        self.get_logger().info("Restored original tmux window names and pane colors.")
+        self._node.get_logger().info("Restored original tmux window names and pane colors.")
 
     def save_window_names(self):
         try:
@@ -57,19 +74,23 @@ class TmuxAlertNode(Node):
             for line in result.stdout.strip().split("\n"):
                 session_window, name = line.rsplit(":", 1)
                 self.original_window_names[session_window] = name
-            self.get_logger().info("Stored original tmux window names.")
+            self._node.get_logger().info("Stored original tmux window names.")
         except subprocess.CalledProcessError as e:
-            self.get_logger().error(f"Failed to list tmux windows: {e.stderr}")
+            self._node.get_logger().error(f"Failed to list tmux windows: {e.stderr}")
 
     def check_condition(self):
         if self.condition_met():
-            self.get_logger().info("Condition met! Triggering alerts...")
+            self._node.get_logger().info("Condition met! Triggering alerts...")
             self.alert_tmux()
             self.play_sound()
 
     def condition_met(self):
-        # Dummy logic – replace with real condition
-        return True
+        if self._battery_percentage is None:
+            self._node.get_logger().info("No Battery Message yet.")
+            return False
+
+        if self._battery_percentage < 75:
+            return True
 
     def alert_tmux(self):
         try:
@@ -103,31 +124,14 @@ class TmuxAlertNode(Node):
                 subprocess.call(["tmux", "select-pane", "-t", pane_id, "-P", "default"])
 
         except subprocess.CalledProcessError as e:
-            self.get_logger().error(f"Failed to get tmux panes: {e.stderr}")
+            self._node.get_logger().error(f"Failed to get tmux panes: {e.stderr}")
 
     def rename_all_windows(self, new_name="!!! ALERT !!!"):
         for win_id in self.original_window_names.keys():
             subprocess.call(["tmux", "rename-window", "-t", win_id, new_name])
 
-        ## Simulate blinking by renaming window rapidly
-
-        #pane_colors = [
-        #("white", "black"),
-        #("black", "white"),
-        #]
-        #
-        #for _ in range(6):  # Blink 3 times
-        #    for fg, bg in pane_colors:
-        #        subprocess.call([
-        #            "tmux", "select-pane", "-P", f"fg={fg},bg={bg}"
-        #        ])
-        #        time.sleep(0.3)
-        #
-        ## Optionally reset to default (you can adjust this)
-        #subprocess.call(["tmux", "select-pane", "-P", "default"])
 
     def play_sound(self):
-        # Replace with actual sound file
         sound_file = "/home/parallels/ros2_ws/src/smarc2/scripts/tmux_alert/resource/siren-alert-96052.mp3"
         try:
             subprocess.Popen([
@@ -141,8 +145,9 @@ class TmuxAlertNode(Node):
             self.get_logger().error(f"Failed to play sound: {e}")
 
 def main(args=None):
-    rclpy.init(args=args)
-    node = TmuxAlertNode()
+    rclpy.init(args=sys.argv)
+    node = rclpy.create_node("AlertNode")
+    alert_node = TmuxAlertNode(node)
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
