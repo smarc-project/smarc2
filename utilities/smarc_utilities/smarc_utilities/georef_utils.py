0dcaf495c3aa63ac8076af3d75236159a854db0e
Li Ling <liling@kth.se>
2025-06-04 13:26:47 +0200

    Add utility function: convert ENU pose to heading

--- utilities/smarc_utilities/smarc_utilities/georef_utils.py
+++ utilities/smarc_utilities/smarc_utilities/georef_utils.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 from geodesy.utm import fromMsg, UTMPoint
-from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import PointStamped, Pose
 from geographic_msgs.msg import GeoPoint
+from std_msgs.msg import Float32
+from tf_transformations import euler_from_quaternion
+import math
 
 def convert_latlon_to_utm(gp: GeoPoint) -> PointStamped:
     """
@@ -29,3 +32,29 @@ def convert_utm_to_latlon(utm: PointStamped) -> GeoPoint:
     msg = utm_pt.toMsg()
 
     return msg
+
+
+def convert_enu_pose_to_heading(enu_pose: Pose) -> Float32:
+    """
+    Convert an ENU pose to a heading in degrees.
+    :param enu_pose: Pose in ENU coordinates
+    :return: Float32 message with compass heading in degrees (0-360)
+    """
+    enu_orientation = enu_pose.orientation
+    rpy_enu = euler_from_quaternion(
+        [enu_orientation.x, enu_orientation.y, enu_orientation.z, enu_orientation.w])
+    yaw = rpy_enu[2]  # Yaw in radians
+
+    # Convert input yaw to degrees
+    yaw_deg = yaw * (180 / math.pi)
+
+    # Convert yaw (ENU) to heading (NED)
+    heading = 90. - yaw_deg
+
+    # Bound to 0 - 360
+    compass_heading = heading % 360.
+
+    compass_heading_msg = Float32()
+    compass_heading_msg.data = compass_heading
+
+    return compass_heading_msg
