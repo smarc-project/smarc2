b115b6b39332df7ee874d1e9e3ba536eca8e3eae
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-06-06 16:15:42 +0200

    starting dji brngup

---
+++ scripts/smarc_bringups/scripts/dji_bringup.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+ROBOT_NAME=Quadrotor
+SESSION=${ROBOT_NAME}_bringup
+
+USE_SIM_TIME=False
+
+# New variables for wasp_bt.launch and wasp_mqtt_agent.launch
+AGENT_TYPE=air
+LEVELS="['sensor','direct_execution']"
+PULSE_RATE=1
+LINK_SUFFIX=""
+
+# create a tmux session with a name
+tmux -2 new-session -d -s $SESSION
+
+
+# create a bunch of windows. These are the "tabs" you'll
+# see at the bottom green line.
+# C-b <NUM> will change to the tab.
+# default window is 0
+
+# PSDK_ROS2_BRIDGE
+tmux new-window -t $SESSION:0 -n 'psdk_bridge'
+tmux rename-window "psdk_bridge"
+
+# the dji_captain
+tmux new-window -t $SESSION:1 -n 'dji_captain'
+tmux rename-window "dji_captain"
+
+# action servers
+tmux new-window -t $SESSION:2 -n 'action_servers'
+tmux rename-window "action_servers"
+
+# bt
+tmux new-window -t $SESSION:3 -n 'bt'
+tmux rename-window "bt"
+
+# mqtt bridge
+tmux new-window -t $SESSION:4 -n 'mqtt_bridge'
+tmux rename-window "mqtt_bridge"
+
+
+# only launch if not the simulator
+if [ "$USE_SIM_TIME" = "True" ]; then
+    tmux select-window -t $SESSION:0
+    tmux send-keys "ros2 launch psdk_ros2_bridge psdk_ros2_bridge.launch" C-m
+
+    tmux select-window -t $SESSION:1
+    tmux send-keys "ros2 launch dji_captain dji_captain.launch" C-m
+fi
+
+tmux select-window -t $SESSION:2
+# action servers when they exist
+
+tmux select-window -t $SESSION:3
+tmux send-keys "ros2 launch wasp_bt wasp_bt.launch robot_name:=$ROBOT_NAME link_suffix:=$LINK_SUFFIX agent_type:=$AGENT_TYPE levels:=$LEVELS pulse_rate:=$PULSE_RATE use_sim_time:=$USE_SIM_TIME" C-m
+
+tmux select-window -t $SESSION:4
+if [ "$USE_SIM_TIME" = "True" ]; then
+    tmux send-keys "ros2 launch str_json_mqtt_bridge mqtt_bridge.launch robot_name:=$ROBOT_NAME domain:=air realsim:=simulation" C-m
+else
+    tmux send-keys "ros2 launch str_json_mqtt_bridge mqtt_bridge.launch robot_name:=$ROBOT_NAME domain:=air realsim:=real" C-m
+fi
+
+
+
+
+
+# Set default window
+tmux select-window -t $SESSION:1
+# attach to the new session
+tmux -2 attach-session -t $SESSION
