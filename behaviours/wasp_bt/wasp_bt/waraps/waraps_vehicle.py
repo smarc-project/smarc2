1455540c78e1edce59239c127c6b4c7e372ef001
NinjaTuna007 <94217791+NinjaTuna007@users.noreply.github.com.>
2025-06-06 15:24:53 +0200

    real lolo bringup

--- behaviours/wasp_bt/wasp_bt/waraps/waraps_vehicle.py
+++ behaviours/wasp_bt/wasp_bt/waraps/waraps_vehicle.py
@@ -188,7 +188,7 @@ def main(args=None):
     # Declare and get parameters with defaults
     node.declare_parameter("agent_type", "air")
     node.declare_parameter("levels", ["sensor", "direct_execution"])
-    node.declare_parameter("pulse_rate", 1)
+    node.declare_parameter("pulse_rate", 1.0) # Hz
     node.declare_parameter("domain", "simulation")
 
     agent_type = node.get_parameter("agent_type").value
