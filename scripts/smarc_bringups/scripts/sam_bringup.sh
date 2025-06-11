2d8713b23b7aa43112cf1312d5b7fae61ad3fc4f
David <ddorner@kth.se>
2025-06-11 17:12:29 +0200

    Updated sam bringup

--- scripts/smarc_bringups/scripts/sam_bringup.sh
+++ scripts/smarc_bringups/scripts/sam_bringup.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
-# Set LOCAL_ROBOT_NAME in your bashrc
+# Set LOCAL_ROBOT_NAME, LOCAL_MQTT_BROKER_IP, and LOCAL_MQTT_BROKER_PORT in your bashrc
 ROBOT_NAME=$LOCAL_ROBOT_NAME
+MQTT_BROKER_IP=$LOCAL_MQTT_BROKER_IP
+MQTT_BROKER_PORT=$LOCAL_MQTT_BROKER_PORT
+
 SESSION=${ROBOT_NAME}_bringup
-USE_SIM_TIME=False
+USE_SIM_TIME=True
+
 # New variables for wasp_bt.launch and wasp_mqtt_agent.launch
 AGENT_TYPE=subsurface
 PULSE_RATE=0.5 # Hz
@@ -13,6 +17,7 @@ else
     REALSIM=real
     LINK_SUFFIX=""
 fi
+
 # create a tmux session with a name
 tmux -2 new-session -d -s $SESSION
 if [ $REALSIM = "simulation" ]
@@ -43,70 +48,54 @@ else
 fi    
 
 # state estimation stuff like pressure->depth, imu->tf etc
-tmux new-window -t $SESSION:0 -n 'dr'
+tmux new-window -t $SESSION:1 -n 'dr'
 tmux rename-window "dr"
-# BT, action servers etc.
-tmux new-window -t $SESSION:1 -n 'bt'
-tmux rename-window "bt"
-# controllers that are "constantly running"
-tmux new-window -t $SESSION:2 -n 'control'
-# connection to different GUIs
-tmux new-window -t $SESSION:3 -n 'gui'
-# utility stuff like dubins planning and lat/lon conversions that other stuff rely on
-tmux new-window -t $SESSION:4 -n 'utils'
-
-# for robot description launch. so we get base_link -> everything else
-tmux new-window -t $SESSION:8 -n 'description'
-# dummy stuff to temporarily let other stuff work
-tmux new-window -t $SESSION:9 -n 'dummies'
-
-# for the mqtt bridge.
-tmux new-window -t $SESSION:10 -n 'mqtt'
-
-
-
-# Now we launch things in each window.
-tmux select-window -t $SESSION:0
+tmux select-window -t $SESSION:1
 #tmux send-keys "ros2 launch sam_dead_reckoning sam_dr_launch.launch robot_name:=$ROBOT_NAME" C-m
 tmux send-keys "echo 'Not launching sam_dead_reckoning sam_dr_launch.launch until someone fixes it!'" C-m
 
-tmux select-window -t $SESSION:1
-tmux send-keys "ros2 launch wasp_bt wasp_bt.launch robot_name:=$ROBOT_NAME" C-m
-
-tmux select-window -t $SESSION:2
-tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME" C-m
+# BT, action servers etc.
+tmux new-window -t $SESSION:2 -n 'bt'
+tmux rename-window "bt"
+# FIXME: Ideally we want to launch the bt here already instead of last to avoid any other confusion.
+#tmux select-window -t $SESSION:2
+#tmux send-keys "ros2 launch wasp_bt wasp_bt.launch robot_name:=$ROBOT_NAME" C-m
 
 # controllers that are "constantly running"
-#tmux new-window -t $SESSION:3 -n 'control'
-#tmux select-window -t $SESSION:3
-#tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME tf_suffix:=_gt" C-m
-#tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME use_sim_time:=$USE_SIM_TIME" C-m
+tmux new-window -t $SESSION:3 -n 'control'
+tmux select-window -t $SESSION:3
+tmux send-keys "ros2 launch sam_diving_controller actionserver.launch robot_name:=$ROBOT_NAME" C-m
 
-# connection to different GUIs
-#tmux new-window -t $SESSION:4 -n 'gui'
-#tmux select-window -t $SESSION:4
-#tmux send-keys "ros2 launch smarc_nodered smarc_nodered.launch robot_name:=$ROBOT_NAME" C-m
+# SMaRC Publisher
+tmux new-window -t $SESSION:4 -n 'smcp'
+tmux select-window -t $SESSION:4
+tmux send-keys "ros2 launch sam_smarc_publisher default.launch robot_name:=$ROBOT_NAME" C-m
 
 # utility stuff like dubins planning and lat/lon conversions that other stuff rely on
 tmux new-window -t $SESSION:5 -n 'utils'
 tmux select-window -t $SESSION:5
 tmux send-keys "ros2 launch smarc_bringups utilities.launch robot_name:=$ROBOT_NAME" C-m
 
-
-# Mostly static stuff that wont be giving much feedback
+# for robot description launch. so we get base_link -> everything else
+tmux new-window -t $SESSION:8 -n 'description'
 tmux select-window -t $SESSION:8
 tmux send-keys "ros2 topic pub -r 1 /$ROBOT_NAME/smarc/vehicle_health std_msgs/msg/Int8 '{data: 0}' " # don't use C-m here, we want to keep the command in the window
+
+# dummy stuff to temporarily let other stuff work
+tmux new-window -t $SESSION:9 -n 'dummies'
 tmux new-window -t $SESSION:9 -n 'emergency_action'
 tmux select-window -t $SESSION:9
 # tmux send-keys "ros2 launch smarc_bringups dummies.launch robot_name:=$ROBOT_NAME" C-m
 tmux send-keys "ros2 launch sam_emergency_action sam_emergency_action.launch robot_name:=$ROBOT_NAME" C-m
 
+# for the mqtt bridge.
+tmux new-window -t $SESSION:10 -n 'mqtt'
 # Set your MQTT Broker IP and Port in your bashrc
 tmux select-window -t $SESSION:10
 tmux send-keys "ros2 launch str_json_mqtt_bridge waraps_bridge.launch broker_addr:=$MQTT_BROKER_IP broker_port:=$MQTT_BROKER_PORT robot_name:=$ROBOT_NAME" C-m
 
 # Launch the wasp_bt LAST, to give action servers time to start publishing heartbeats
-tmux select-window -t $SESSION:1
+tmux select-window -t $SESSION:2
 tmux send-keys "ros2 launch wasp_bt wasp_bt.launch robot_name:=$ROBOT_NAME agent_type:=$AGENT_TYPE pulse_rate:=$PULSE_RATE use_sim_time:=$USE_SIM_TIME" C-m
 
 USERNAME=$(whoami)
