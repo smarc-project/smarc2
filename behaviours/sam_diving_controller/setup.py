986e15d7f1ed3fd1bf6d16c0a99b087b8f3caa86
CSimonSp <sspang00@gmail.com>
2025-06-25 12:48:45 +0200

    Added NMPC as launch file and in setup

--- behaviours/sam_diving_controller/setup.py
+++ behaviours/sam_diving_controller/setup.py
@@ -29,6 +29,7 @@ setup(
             'action_server_diving = sam_diving_controller.Node:action_server',
             'setpoint = sam_diving_controller.SetpointNode:main',
             'joy_depth = sam_diving_controller.JoyNode:joy_depth'
+            'nmpc_diving = sam_diving_controller.Node:nmpc_depth'
         ],
     },
 )
