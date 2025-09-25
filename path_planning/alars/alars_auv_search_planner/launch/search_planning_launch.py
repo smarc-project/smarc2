47367c2f4b793f6184fcc310201283604fc8d0fd
Francisco <franmira2003@gmail.com>
2025-09-25 12:14:59 +0200

    namespace as launch argument

--- path_planning/alars/alars_auv_search_planner/launch/search_planning_launch.py
+++ path_planning/alars/alars_auv_search_planner/launch/search_planning_launch.py
@@ -8,16 +8,19 @@ from launch_ros.substitutions import FindPackageShare
 # Main parameters: change them manually or via CLI
 MODE = "as"  # sim, srv, as
 PLANNER = "spiral"  # spiral, greedy, astar, apf
+NAMESPACE = "/Quadrotor"
 
 def generate_launch_description():
 
     # ---- frequently changed params as launch arguments ...
+    namespace_arg = DeclareLaunchArgument('namespace', default_value=NAMESPACE)
     mode_arg = DeclareLaunchArgument('mode', default_value=MODE)
     path_planner_arg = DeclareLaunchArgument('path_planner', default_value=PLANNER)
     sam_init_pos_arg = DeclareLaunchArgument('sam_init_pos', default_value='[1300.0, 1153.0]')
-    drone_init_pos_arg = DeclareLaunchArgument('drone_init_pos', default_value='[100.0, -150.0]')
+    drone_init_pos_arg = DeclareLaunchArgument('drone_init_pos', default_value='[5.0, -5.0]')
 
     # ... and as node params
+    namespace = LaunchConfiguration('namespace')
     mode = LaunchConfiguration('mode')
     path_planner = LaunchConfiguration('path_planner')
     sam_init_pos = LaunchConfiguration('sam_init_pos')
@@ -35,11 +38,12 @@ def generate_launch_description():
     as_node = Node(
         package='alars',
         executable="alars_search_action_server",
-        namespace='Quadrotor',
+        namespace='M350',
         output='screen',
         parameters=[
             config_file,
             {
+                'namespace': namespace,
                 'mode': mode,
                 'path_planner': path_planner,
                 'sam.init_pos': sam_init_pos,
@@ -53,11 +57,12 @@ def generate_launch_description():
     default_node = Node(
         package='alars_auv_search_planner',
         executable="search_planner_controller",
-        namespace='Quadrotor',
+        namespace='M350',
         output='screen',
         parameters=[
             config_file,
             {
+                'namespace': namespace,
                 'mode': mode,
                 'path_planner': path_planner,
                 'sam.init_pos': sam_init_pos,
@@ -68,6 +73,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        namespace_arg,
         mode_arg,
         path_planner_arg,
         sam_init_pos_arg,
@@ -75,6 +81,7 @@ def generate_launch_description():
 
         # Debug logging
         LogInfo(msg=["[Launch] mode argument = ", mode]),
+        LogInfo(msg=["[Launch] namespace argument = ", namespace]),
 
         as_node,
         default_node,
