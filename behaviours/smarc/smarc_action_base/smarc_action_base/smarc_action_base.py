653dcb7b3872fe355aca51e2aadef145d0c46d01
Ozer Ozkahraman <ozero@kth.se>
2026-05-28 14:39:34 +0200

    undo hack action base

--- behaviours/smarc/smarc_action_base/smarc_action_base/smarc_action_base.py
+++ behaviours/smarc/smarc_action_base/smarc_action_base/smarc_action_base.py
@@ -372,7 +372,12 @@ class SMARCActionClient(abc.ABC):
     def _setup(self, num_iters: int = 3, timeout: float = 1.0) -> bool:
         server_status = False
         iters = 0
-        preserve_state = False
+        preserve_state = self._state in (
+            ActionClientState.SENT,
+            ActionClientState.ACCEPTED,
+            ActionClientState.RUNNING,
+            ActionClientState.CANCELLING,
+        )
         while not server_status and iters < num_iters:
             iters += 1
             self._loginfo("Waiting for server to start.")
