88cf870e76e1d58fc8e0e9e5bf7e84f32a4cf152
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2026-06-26 17:22:42 +0200

    gentler action server a little more gentler

--- behaviours/smarc/smarc_action_base/smarc_action_base/gentler_action_server.py
+++ behaviours/smarc/smarc_action_base/smarc_action_base/gentler_action_server.py
@@ -42,29 +42,72 @@ class GentlerActionServer(SMARCActionServer):
         self._give_feedback :      Callable[[], str]         = give_feedback
         self._loop_frequency :     float                     = loop_frequency
 
+        self._user_failure = False # will be set to true if a user-given callback fails
+
 
     def goal_callback(self, goal_request) -> GoalResponse:
-        return  GoalResponse.ACCEPT if self._on_goal_received(json.loads(goal_request.goal.data)) else GoalResponse.REJECT
+        if self._user_failure:
+            self._node.get_logger().error("User-supplied callback previously failed, rejecting new goal request. Fix your stuff first and restart!")
+            return GoalResponse.REJECT
+        
+        try:
+            user_result = self._on_goal_received(json.loads(goal_request.goal.data))
+        except Exception as e:
+            self._node.get_logger().error(f"Exception occurred in user-supplied goal callback while processing goal request:\n {e}")
+            self._user_failure = True
+            return GoalResponse.REJECT
+        
+        return  GoalResponse.ACCEPT if user_result else GoalResponse.REJECT
 
 
     def cancel_callback(self, goal_handle: ServerGoalHandle) -> CancelResponse:
-        return  CancelResponse.ACCEPT if self._on_cancel_received() else CancelResponse.REJECT
-    
+        try:
+            user_result = self._on_cancel_received()
+        except Exception as e:
+            self._node.get_logger().error(f"Exception occurred in user-supplied cancel callback while processing cancel request for goal:\n {e}")
+            self._user_failure = True
+            return CancelResponse.REJECT
+        return  CancelResponse.ACCEPT if user_result else CancelResponse.REJECT
+        
+        
 
     def execution_callback(self, goal_handle) -> ActionResult:
         result_msg = BaseAction.Result()
         feedback_msg = BaseAction.Feedback()
 
-        self._prepare_loop()
+        def cancel():
+            result_msg.success = False
+            goal_handle.canceled()
+            self._user_failure = True
+            return result_msg
 
-        rate = self._node.create_rate(self._loop_frequency)
+        try:
+            self._prepare_loop()
+        except Exception as e:
+            self._node.get_logger().error(f"Exception occurred in user-supplied prepare_loop callback while preparing for goal execution:\n {e}")
+            return cancel()
+
+        try:
+            rate = self._node.create_rate(self._loop_frequency)
+        except:
+            self._node.get_logger().error(f"Exception occurred while creating rate, user-supplied loop_frequency: {self._loop_frequency}")
+            return cancel()
 
         while rclpy.ok() and not goal_handle.is_cancel_requested:
-            loop_status : bool|None = self._loop_inner()
+            try:
+                loop_status : bool|None = self._loop_inner()
+            except Exception as e:
+                self._node.get_logger().error(f"Exception occurred in user-supplied loop inner callback:\n {e}")
+                return cancel()
+            
             if loop_status is None:
                 # loop continues, not successful or failed yet
-                feedback_msg.feedback.data = self._give_feedback()
-                goal_handle.publish_feedback(feedback_msg)
+                try:
+                    feedback_msg.feedback.data = self._give_feedback()
+                    goal_handle.publish_feedback(feedback_msg)
+                except Exception as e:
+                    self._node.get_logger().error(f"Exception occurred in user-supplied give_feedback callback:\n {e}")
+                    return cancel()
             else:
                 result_msg.success = loop_status
                 rate.destroy()
@@ -76,9 +119,8 @@ class GentlerActionServer(SMARCActionServer):
             
             rate.sleep()
         
-        result_msg.success = False
-        goal_handle.canceled()
-        return result_msg
+        return cancel()
+
 
 
         
