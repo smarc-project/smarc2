671bf4788d71333fd08a9a41c6df28748c02fa24
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2026-08-27 22:15:29 +0200

    less spam in logs. more alars submodules

--- .github/workflows/print_tmux_logs.sh
+++ .github/workflows/print_tmux_logs.sh
@@ -2,6 +2,6 @@ for file in "$GITHUB_WORKSPACE"/tmux-logs/*; do
     [ -f "$file" ] || continue
 
     echo "::group::TMUX PANE: $(basename "$file")"
-    cat "$file"
+    tail -n 50 "$file"
     echo "::endgroup::"
 done
