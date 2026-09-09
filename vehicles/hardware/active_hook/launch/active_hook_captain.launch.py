41edbed5df61ddbe97b749cc26cbb8e72ecaf277
ulvixz <aliulvi4103@hotmail.com>
2026-09-09 12:05:39 +0200

    implemented joy->twist->manual_control into active_hook_captain

--- vehicles/hardware/active_hook/launch/active_hook_captain.launch.py
+++ vehicles/hardware/active_hook/launch/active_hook_captain.launch.py
@@ -1,16 +1,8 @@
-import os
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-
-from active_hook_msgs.msg import Topics as ActiveHookTopics
 
 
 def generate_launch_description():
-
-    package_share = get_package_share_directory('active_hook')
-    config_file = os.path.join(package_share, 'config', 'ps5_rov.yaml')
 
     joy_node = Node(
         package='joy',
@@ -29,23 +21,10 @@ def generate_launch_description():
         output='screen'
     )
 
-    teleop_node = Node(
-        package='teleop_twist_joy',
-        executable='teleop_node',
-        name='teleop_twist_joy_node',
-        namespace='ActiveHook',
-        output='screen',
-        parameters=[config_file],
-        remappings=[
-            ('joy', ActiveHookTopics.JOY_ROV_TOPIC),
-            ('cmd_vel', ActiveHookTopics.AUTONOMY_CMD_VEL_TOPIC)
-        ]
-    )
-
     mavros_node = Node(
         package='mavros',
         executable='mavros_node',
-        namespace='ActiveHook/mavros',   # <- iç içe namespace, tek satırda
+        namespace='ActiveHook/mavros',   
         output='screen',
         parameters=[{
             'fcu_url': 'udp://0.0.0.0:14551@',
@@ -59,6 +38,5 @@ def generate_launch_description():
     return LaunchDescription([
         joy_node,
         captain_node,
-        teleop_node,
         mavros_node
     ])
